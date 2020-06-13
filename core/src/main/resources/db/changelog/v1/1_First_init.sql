026256a6124c6f5b34bed423667405d986328149
IVTregub <tregiven@gmail.com>
2020-06-13 21:51:16 +0400

    TME-1.1 Create table users

--- core/src/main/resources/db/changelog/v1/1_First_init.sql
+++ core/src/main/resources/db/changelog/v1/1_First_init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `trustmeenglish`.`users` (
-    `id` BIGINT(20) NOT NULL,
+    `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(45) NOT NULL,
     `password` VARCHAR(45) NOT NULL,
-     PRIMARY KEY (`id`),
-     UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
