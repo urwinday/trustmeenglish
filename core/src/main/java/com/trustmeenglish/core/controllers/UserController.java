7ccec79954ecbc37544c0d5a416b2c6050dbb762
IVTregub <tregiven@gmail.com>
2020-06-14 16:46:23 +0400

    TME-1.1 MapStruct Entity to DTO

--- core/src/main/java/com/trustmeenglish/core/controllers/UserController.java
+++ core/src/main/java/com/trustmeenglish/core/controllers/UserController.java
@@ -1,17 +1,13 @@
 package com.trustmeenglish.core.controllers;
 
-import com.trustmeenglish.core.dto.RoleDTO;
 import com.trustmeenglish.core.dto.UserDTO;
-import com.trustmeenglish.core.model.Role;
+import com.trustmeenglish.core.mappers.UserMapper;
 import com.trustmeenglish.core.model.User;
 import com.trustmeenglish.core.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,27 +18,7 @@ public class UserController {
     @GetMapping("/hello/{id}")
     public UserDTO sayHello(@PathVariable Long id) {
         User user = userService.getUser(id);
-
-        return toDTO(user);
+        return UserMapper.INSTANCE.toDTO(user);
     }
 
-    private UserDTO toDTO(User user) {
-        Set<RoleDTO> roleDTOS = toDTOs(user.getRoles());
-        return UserDTO.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .roles(roleDTOS)
-                .build();
-    }
-
-    private Set<RoleDTO> toDTOs(Set<Role> role) {
-        return role.stream().map(role1 -> toDTO(role1)).collect(Collectors.toSet());
-    }
-
-    private RoleDTO toDTO(Role role) {
-        return RoleDTO.builder()
-                .id(role.getId())
-                .name(role.getName())
-                .build();
-    }
 }
